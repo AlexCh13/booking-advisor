13da9efaeef53807aef66bf5ec1c5c2a190921d4
Alex <alex.chirita@outlook.com>
2018-02-20 17:37:45 +0200

    done

--- src/db.js
+++ src/db.js
@@ -1,31 +1,41 @@
-var PropertiesReader = require('properties-reader');
-var properties = PropertiesReader('./properties_server.txt');
-
-const PORT = properties.get('port');
-
-const URL = properties.get('url');
-const USER_MONGO = properties.get('userMongo');
-const PASSWORD_MONGO = properties.get('passwordMongo');
-const SERVER_MONGO = properties.get('serverMongo');
-const PORT_MONGO = properties.get('portMongo');
-const DB_MONGO = properties.get('dbMongo');
-const DB_IP_NETWORK = properties.get('addressIP');
+//var PropertiesReader = require('properties-reader');
+//var properties = PropertiesReader('./properties_server.txt');
 
 
-var database;
-var myCollectionHouses;
 
+//const URL = properties.get('url');
+//const USER_MONGO = properties.get('userMongo');
+//const PASSWORD_MONGO = properties.get('passwordMongo');
+//const SERVER_MONGO = properties.get('serverMongo');
+//const PORT_MONGO = properties.get('portMongo');
+//const DB_MONGO = properties.get('dbMongo');
 var cors = require('cors'); 
 var express = require('express');
 var http = require('http');
 var mongo =  require('mongodb');
 var bodyParser = require('body-parser');
+var mb = require('morgan-body');
+
+
+const DB_IP_NETWORK = process.env.OPENSHIFT_NODEJS_IP   //properties.get('addressIP'); - 127.0.0.1
+const PORT = process.env.OPENSHIFT_NODEJS_PORT          //properties.get('port');      - 8080
+
+var connection_string = '127.0.0.1:27017/bookingadvisor';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+}
+
+var database;
+var myCollectionHouses;
 
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-var mb = require('morgan-body');
 
 mb(app);
 
@@ -43,15 +53,15 @@ next();
 
 var MongoClient = mongo.MongoClient;
 
-var url_for_connect = URL + USER_MONGO + ":" + PASSWORD_MONGO + "@" + SERVER_MONGO + ":" + PORT_MONGO
- + "/" + DB_MONGO;
+// var url_for_connect = URL + USER_MONGO + ":" + PASSWORD_MONGO + "@" + SERVER_MONGO + ":" + PORT_MONGO + "/" + DB_MONGO;
  
+var url_for_connect = 'mongodb://' + connection_string;
 
 // connect with authentication or not
 function try_to_connect (err, db) {
     if (err) { 
-        console.log(err.name + " - " + err.message);
         console.log("Error connecting to MongoDB. Aborting...");
+        throw err;
         process.exit()
     }
     else {
@@ -59,8 +69,7 @@ function try_to_connect (err, db) {
 
         housesCollection = database.collection('Houses');
         userCollection = database.collection('Users');
-
-
+        
         console.log("Connected to Mongo with successed !");
     }
 }
